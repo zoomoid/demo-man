98241125c27b91934fccac889deac231496242d6
Alexander Bartolomey <occloxium@gmail.com>
2020-07-02 00:24:22 +0200

    watchdog: refactor large portions of logging code

--- watchdog/index.js
+++ watchdog/index.js
@@ -13,12 +13,12 @@ const apiEndpoint = process.env.API_ENDPOINT || 'http://demo-api/api/v1/demo'
 
 const url = JSON.parse(process.env.PUBLIC_PATH) || {
   prefix: 'https',
-  hostname: 'files.zoomoid.de', // Use internal fileserver instead of public one!
-  dir: ``, // needs to be slash-terminated!
+  hostname: 'files.zoomoid.de',
+  dir: '',
 };
 
 if(!process.env.TOKEN){
-  logger.warn(`No API token provided. Protected routes will not work properly.`);
+  logger.warn('No API token provided. Protected routes will not work properly.');
 }
 
 const token = process.env.TOKEN;
@@ -28,28 +28,28 @@ const token = process.env.TOKEN;
  * 
  * General guidelines for using this watchdog: 
  * We listen to all changes in 2 layers of depth, i.e., we only get notified for changes to the relative root
- * and one layer below. This enables us to group tracks together to albums, as we can create folders and
- * watch for file changes inside the new folders.
- * Newly created folders are treated as albums being created.
- * Files created inside these folders are treated as tracks being created.
+ * and one layer below. This enables us to group tracks together to albums, as we can create directories and
+ * watch for file changes inside the new directories.
+ * Newly created directories are treated as albums being created.
+ * Files created inside these directories are treated as tracks being created.
  * 
- * The same applies to deletion of said folders/files.
+ * The same applies to deletion of said directories/files.
  * 
  * Hierarchy:
  * 
  * ./ (relative root; determined by "VOLUME")
- * |- album1
+ * |- namespace
  *    |- track1.mp3
- *    |- track2.wav
+ *    |- track2.mp3
  *    |- track3.mp3
- * |- album2
+ * |- namespace
  *    |- track1.mp3
- *    |- track2.flac
- * |- album3
+ *    |- track2.mp3
+ * |- namespace
  * 
  * This is a valid file tree for our example watchdog 
  */
-const volume = process.env.VOLUME || `.`; // needs to be slash-terminated!
+const volume = process.env.VOLUME || '.';
 
 /** FILE Watcher */
 const fileWatcher = chokidar.watch(`${volume}/[a-zA-Z0-9]*/*.mp3`, {
@@ -76,7 +76,7 @@ const folderWatcher = chokidar.watch(`${volume}/`, {
   depth: 1,
 });
 
-logger.info(`Watching directory`, `volume`, volume);
+logger.info('Watching directory', 'volume', volume);
 
 /** 
  * Garbage collector
@@ -84,55 +84,53 @@ logger.info(`Watching directory`, `volume`, volume);
  * This file watcher removes any files and directories created in the process of SFTP handshakes by FileZilla
  */
 const garbageCollector = chokidar.watch(['.cache', '.gnupg', 'private-keys-v1.d']);
-garbageCollector.on("addDir", async (path) => {
-  logger.info(`Cleaning up trash directories`, `directory`, path);
+garbageCollector.on('addDir', async (path) => {
+  logger.info('Cleaning up trash directories', 'directory', path);
   fs.rmdir(path, {recursive: true}, (err) => {
     if (err) {
-      logger.error(`Error on rmdir w/ recursive option`, `error`, err);
+      logger.error('Error on rmdir w/ recursive option', 'error', err);
       return
     } 
-    logger.info(`Finish cleanup round`, `directory`, path);
+    logger.info('Finish cleanup round', 'directory', path);
   });
-});
-
-/**
- * Awaits directory addition and requests creation of a new namespace from the API server
- */
-folderWatcher.on('addDir', async path => {
-  if(path == `${volume}/`){
-    logger.info(`Skipping docker volume mount event`, `directory`, path);
-    return
-  }
-  logger.info(`Observed addition of directory`, `directory`, path, `timestamp`, new Date().toLocaleString('de-DE'));
-  let folder = p.basename(path);
-  logger.info(`Created new folder`, `path`, path, `folder`, folder);
-  try {
-    await postAlbumToAPI(folder);
-  } catch (err) {
-    logger.error('Received error from API server', `event`, `addDir`, `path`, path, `error`, err);
-    logger.info('Reverting changes to file system', `path`, path);
-    fs.unlink(path, (err) => {
-      logger.error('I/O Error on unlink. Exiting...', `error`, err);
-      process.exit(1);
-    });
-  }
 });
 
 /**
  * Awaits new files to be added and requests insertion into the namespace from the API server
  */
 fileWatcher.on('add', async path => {
-  logger.info(`File has been added`, `file`, path, `timestamp`, new Date().toLocaleString('de-DE'));
-  const reducedFilename = path.replace(`${volume}/`,``); // strips the volume mount prefix from the filename
-  let meta = await readMetadata(reducedFilename);
   try {
-    logger.info(`Requesting insertion of indexed audio file`, `filename`, p.basename(reducedFilename), `publicUrl`, meta.url);
-    await postTrackToAPI(meta);
+    logger.info('File added', 'file', path, 'timestamp', new Date().toLocaleString('de-DE'));
+    const reducedFilename = path.replace(`${volume}/`,''); // strips the volume mount prefix from the filename
+    let track = await readMetadata(reducedFilename);
+    await postTrackToAPI(track);
   } catch (err) {
-    logger.error('Received error from API server', `event`, `add`, `path`, path, `error`, err);
-    logger.info('Reverting changes to file system', `path`, path);
+    logger.error('Received error from API server', 'event', 'add', 'path', path, 'error', err);
+    logger.info('Reverting changes to file system', 'path', path);
     fs.unlink(path, (err) => {
-      logger.error('I/O Error on unlink. Exiting...', `error`, err);
+      logger.error('I/O Error on unlink. Exiting...', 'error', err);
+      process.exit(1);
+    });
+  }
+});
+
+/**
+ * Awaits directory addition and requests creation of a new namespace from the API server
+ */
+folderWatcher.on('addDir', async path => {
+  try {
+    if(path == `${volume}/`){
+      logger.info('Skipping docker volume mount event', 'directory', path);
+      return
+    }
+    logger.info('Directory added', 'directory', path, 'timestamp', new Date().toLocaleString('de-DE'));
+    let namespace = p.basename(path);
+    await postNamespaceToAPI(namespace);
+  } catch (err) {
+    logger.error('Received error from API server', 'event', 'addDir', 'path', path, 'error', err);
+    logger.info('Reverting changes to file system', 'path', path);
+    fs.unlink(path, (err) => {
+      logger.error('I/O Error on unlink. Exiting...', 'error', err);
       process.exit(1);
     });
   }
@@ -142,13 +140,13 @@ fileWatcher.on('add', async path => {
  * Watcher for file deletions
  */
 fileWatcher.on('unlink', async path => {
-  const reducedFilename = path.replace(`${volume}/`,``); // strips the volume mount prefix from the filename
-  logger.info(`File has been removed`, `file`, reducedFilename, `time`, new Date().toLocaleString('de-DE'));
+  const reducedFilename = path.replace(`${volume}/`,''); // strips the volume mount prefix from the filename
+  logger.info('File removed', 'file', reducedFilename, 'time', new Date().toLocaleString('de-DE'));
   // API Server querys for full path on delete request
   try {
     await removeTrackFromAPI(reducedFilename);
   } catch (err) {
-    logger.error('Received error from API server. Manual assessment required', `event`, `unlink`, `path`, path,  `error`, err);
+    logger.error('Received error from API server. Manual assessment required', 'event', 'unlink', 'path', path,  'error', err);
   }
 });
 
@@ -156,13 +154,13 @@ fileWatcher.on('unlink', async path => {
  * Watcher for album deletions
  */
 folderWatcher.on('unlinkDir', async path => {
-  const reducedFilename = path.replace(`${volume}/`,``); // strips the volume mount prefix from the filename
-  logger.info(`Directory has been removed`, `file`, reducedFilename, `time`, new Date().toLocaleString('de-DE'));
+  const reducedFilename = path.replace(`${volume}/`,''); // strips the volume mount prefix from the filename
+  logger.info('Directory removed', 'directory', reducedFilename, 'time', new Date().toLocaleString('de-DE'));
   // API Server querys for full path on delete request
   try {
-    await removeAlbumFromAPI(reducedFilename)
+    await removeNamespaceFromAPI(reducedFilename)
   } catch (err) {
-    logger.error('Received error from API server. Manual assessment required.', `event`, `unlinkDir`, `error`, err);
+    logger.error('Received error from API server. Manual assessment required.', 'event', 'unlinkDir', 'error', err);
   }
 });
 
@@ -170,13 +168,13 @@ folderWatcher.on('unlinkDir', async path => {
  * Requests creation of a new track
  * @param {*} data track data to post to the API
  */
-async function postTrackToAPI(data) {
-  logger.info(`Requesting addition of track`, `file`, data.title, `ep`, apiEndpoint);
-  const resp = await post(`${apiEndpoint}/file`, data);
+async function postTrackToAPI(track) {
+  logger.info('Adding track...', 'track', track.filename, 'endpoint', apiEndpoint);
+  const resp = await post(`${apiEndpoint}/track`, {'track': track});
   if (resp && resp.status == 200) {
-    logger.info(`Successfully posted track to API`, `response`, resp);
+    logger.info('Added track', 'track', track.filename, 'response', resp);
   } else {
-    logger.error(`Received error status from API`, `response`, resp);
+    logger.error('Received error status from API', 'track', track.filename, 'level', 'postTrackToAPI', 'response', resp);
   }
 }
 
@@ -184,14 +182,13 @@ async function postTrackToAPI(data) {
  * Requests creation of a new album
  * @param {string} album album title
  */
-async function postAlbumToAPI(album) {
-  logger.info(`Requesting addition of album`, `file`, album, `ep`, apiEndpoint);
-
-  const resp = await post(`${apiEndpoint}/folder`, {'album': album});
+async function postNamespaceToAPI(ns) {
+  logger.info('Adding namespace...', 'namespace', ns, 'endpoint', apiEndpoint);
+  const resp = await post(`${apiEndpoint}/namespace`, {'namespace': ns});
   if (resp && resp.status == 200) {
-    logger.info(`Successfully posted album to API`, `response`, resp);
+    logger.info('Added namespace', 'namespace', ns, 'response', resp);
   } else {
-    logger.error(`Received error status from API`, `response`, resp);
+    logger.error('Received error status from API', 'namespace', ns, 'level', 'postNamespaceToAPI', 'response', resp);
   }
 }
 
@@ -200,13 +197,12 @@ async function postAlbumToAPI(album) {
  * @param {string} path path of the audio file to query the DB with
  */
 async function removeTrackFromAPI(path) {
-  const resp = await del(`${apiEndpoint}/file`, {'path': path});
-  logger.info(`Requesting deletion of indexed track`, `file`, path, `db`, url, `ep`, apiEndpoint);
-
+  logger.info('Deleting track...', 'track', path, 'endpoint', apiEndpoint);
+  const resp = await del(`${apiEndpoint}/track`, {'path': path});
   if (resp && resp.status == 200) {
-    logger.info(`Successfully deleted track from API`, `response`, resp);
+    logger.info('Deleted track', 'track', `${p.basename(path)}`, 'response', resp);
   } else {
-    logger.error(`Received error status from API`, `response`, resp);
+    logger.error('Received error status from API', 'level', 'removeTrackFromAPI', 'response', resp);
   }
 }
 
@@ -214,14 +210,13 @@ async function removeTrackFromAPI(path) {
  * Requests deletion of a previously added album
  * @param {*} path directory name that we need to delete
  */
-async function removeAlbumFromAPI(path) {
-  const resp = await del(`${apiEndpoint}/folder`, {'path': path});
-  logger.info(`Requesting deletion of indexed album`, `file`, path, `db`, url, `ep`, apiEndpoint);
-
+async function removeNamespaceFromAPI(ns) {
+  logger.info('Deleting namespace...', 'namespace', ns,'endpoint', apiEndpoint);
+  const resp = await del(`${apiEndpoint}/namespace`, {'namespace': ns});
   if (resp && resp.status == 200) {
-    logger.info(`Successfully deleted album from API`, `response`, resp);
+    logger.info('Deleted namespace', 'namespace', ns, 'response', resp);
   } else {
-    logger.error(`Received error status from API`, `response`, resp);
+    logger.error('Received error status from API', 'level', 'removeNamespaceFromAPI', 'response', resp);
   }
 }
 
@@ -264,15 +259,15 @@ async function __request(method, ep, data){
       });
       return resp;
     } else {
-      logger.warn(`Dry run! Not sending request to API server`, `endpoint`, `${ep}`, `data`, data);
+      logger.warn('Dry run! Not sending request to API server', 'endpoint', `${ep}`, 'data', data);
       return {
-        "success": true,
-        "note": "dry_run",
-        "data": data,
+        'success': true,
+        'note': 'dry_run',
+        'data': data,
       }
     }
   } catch (err) {
-    logger.error(`Received error response from API server`, `response`, err, "endpoint", `${ep}`);
+    logger.error('Received error from API server', 'level', '__request', 'response', err, 'endpoint', `${ep}`);
   }
 }
 
@@ -283,49 +278,48 @@ async function __request(method, ep, data){
 async function readMetadata(path){
   console.log(path);
   try {
-    logger.info(`Reading IDv3 off of audio file`, `path`, path);
+    logger.info('Reading IDv3 off of audio file', 'path', path);
     const src = await metadata.parseFile(p.join(volume, path));
-
-    logger.info(`Parsed audio file metadata`,  `dirname`, p.dirname(path), `filename`, p.basename(path));
-    let mimeType = "";
-    let abspath = "";
+    logger.info('Parsed audio file metadata', 'dirname', p.dirname(path), 'filename', p.basename(path));
+    let mimeType = '';
+    let abspath = '';
     let cover = {};
     if(src.common.picture){
       mimeType = src.common.picture[0].format;
-      abspath = p.join(volume, p.dirname(path), `cover.${mimeType.replace("image/", "")}`);
-      logger.info("Writing cover to file", "filename", abspath, "mimeType", mimeType);  
+      abspath = p.join(volume, p.dirname(path), `cover.${mimeType.replace('image/', '')}`);
+      logger.info('Writing cover to file', 'filename', abspath, 'mimeType', mimeType);  
       fs.writeFileSync(abspath, src.common.picture[0].data);
       cover = {
-        "mimeType": mimeType,
-        "public_url": `${url.prefix}://${url.hostname}/${url.dir}${p.basename(p.dirname(path))}${p.sep}cover.${mimeType.replace("image/", "")}`
+        'mimeType': mimeType,
+        'public_url': `${url.prefix}://${url.hostname}/${url.dir}${p.basename(p.dirname(path))}${p.sep}cover.${mimeType.replace('image/', '')}`
       };
     } else {
-      logger.warn(`Audio file metadata has no cover yet, omitting for now`, `path`, `${path}`);
+      logger.warn('Audio file metadata has no cover yet, omitting for now', 'path', `${path}`);
     }
-    logger.info(`Read metadata off of audio file`, `path`, path);
+    logger.info('Read metadata off of audio file', 'path', path);
     return {
-      "year": src.common.year,
-      "track": src.common.track,
-      "title": src.common.title,
-      "artist": src.common.artist,
-      "albumartist": src.common.albumartist,
-      "album": src.common.album,
-      "genre": src.common.genre,
-      "composer": src.common.composer,
-      "comment": src.common.comment,
-      "bpm": src.common.bpm,
-      "type": src.format.container,
-      "duration": src.format.duration,
-      "lossless": src.format.lossless,
-      "bitrate": src.format.bitrate,
-      "cover": cover,
-      "path": path, // full path of file INSIDE volume
-      "filename": p.basename(path), // this gets us the last element of the array inline
-      "namespace": p.basename(p.dirname(path)),
-      "url": `${url.prefix}://${url.hostname}/${url.dir}${p.basename(p.dirname(path))}${p.sep}${p.basename(path)}` // this monster contains the final shareable url on the webserver
+      'year': src.common.year,
+      'track': src.common.track,
+      'title': src.common.title,
+      'artist': src.common.artist,
+      'albumartist': src.common.albumartist,
+      'album': src.common.album,
+      'genre': src.common.genre,
+      'composer': src.common.composer,
+      'comment': src.common.comment,
+      'bpm': src.common.bpm,
+      'type': src.format.container,
+      'duration': src.format.duration,
+      'lossless': src.format.lossless,
+      'bitrate': src.format.bitrate,
+      'cover': cover,
+      'path': path, // full path of file INSIDE volume, this can be sent to the waveman
+      'filename': p.basename(path),
+      'namespace': p.basename(p.dirname(path)),
+      'url': `${url.prefix}://${url.hostname}/${url.dir}${p.basename(p.dirname(path))}${p.sep}${p.basename(path)}`
     }    
   } catch (err) {
     console.log(err);
-    logger.error("Error while parsing audio metadata", "error", err, `file`, path);
+    logger.error('Error while parsing audio metadata', 'error', err, 'file', path, 'level', 'readMetadata');
   }
 }
