adaa9b554a7d586d29aa50028a226035726653ca
Alexander Bartolomey <occloxium@gmail.com>
2020-08-16 19:43:13 +0200

    api: namespace: adopt logger API change and promise change
    Instead of async/await, use promise chains. This is personal preference,
    because I hate try/catch blocks

--- api/lib/namespace/index.js
+++ api/lib/namespace/index.js
@@ -1,5 +1,5 @@
 const { guard, db } = require("../../util/");
-const logger = require("@zoomoid/log");
+const logger = require("@zoomoid/log").v2;
 const { api } = require("../../endpoints");
 
 module.exports = function (router) {
@@ -8,183 +8,171 @@ module.exports = function (router) {
     /**
      * ADD new album to API
      */
-    .post(guard, async (req, res, next) => {
-      try {
-        const namespace = {
-          type: "Namespace",
-          name: req.body.namespace,
-        };
-        const resp = await db.get().insertOne(namespace);
-        logger.info(
-          "Added new namespace",
-          "level",
-          "POST /namespace",
-          "namespace",
-          namespace.name
-        );
-        res.status(200).json({
-          response: resp,
+    .post(guard, (req, res) => {
+      const namespace = {
+        type: "Namespace",
+        name: req.body.namespace,
+      };
+      db.get()
+        .insertOne(namespace)
+        .then((resp) => {
+          logger.info("Added new namespace", {
+            in: "POST /namespace",
+            namespace: namespace.name,
+          });
+          res.status(200).json({
+            response: resp,
+          });
+        })
+        .catch((err) => {
+          logger.error("Received error from MongoDB", {
+            in: "POST /namespace",
+            error: err,
+          });
+          res.status(500).send("Internal Server Error");
         });
-      } catch (err) {
-        logger.error(
-          "Received error from MongoDB",
-          "level",
-          "POST /namespace",
-          "response",
-          err
-        );
-        next(err);
-      }
     })
     /**
      * DELETE album from API
      */
-    .delete(guard, async (req, res, next) => {
-      try {
-        const path = req.body.namespace;
-        const c = db.get();
-        const resp = await Promise.all([
-          c.deleteMany({ type: "Track", namespace: path }),
-          c.deleteMany({ type: "Namespace", name: path }),
-          c.deleteMany({ type: "Waveform", namespace: path }),
-        ]);
-        logger.info(
-          "Deleted album",
-          "level",
-          "DELETE /namespace",
-          "namespace",
-          `${req.body.namespace}`
-        );
-        res.status(200).json({
-          response: resp,
+    .delete(guard, (req, res) => {
+      const path = req.body.namespace;
+      const c = db.get();
+      const resp = Promise.all([
+        c.deleteMany({ type: "Track", namespace: path }),
+        c.deleteMany({ type: "Namespace", name: path }),
+        c.deleteMany({ type: "Waveform", namespace: path }),
+      ])
+        .then(() => {
+          logger.info("Deleted album", {
+            in: "DELETE /namespace",
+            namespace: `${req.body.namespace}`,
+          });
+          res.status(200).json({
+            response: resp,
+          });
+        })
+        .catch((err) => {
+          logger.error("Received error from MongoDB", {
+            in: "DELETE /namespace",
+            error: err,
+          });
+          res.status(500).send("Internal Server Error");
         });
-      } catch (err) {
-        logger.error(
-          "Received error from MongoDB",
-          "level",
-          "DELETE /namespace",
-          "response",
-          err
-        );
-        next(err);
-      }
     });
 
   /**
    * GET all namespaces/albums
    */
-  router.get("/", async (req, res, next) => {
-    try {
-      let resp = await db.get().find({ type: "Namespace" }).toArray();
-      resp = resp.map((e) => {
-        e.url = `${api.url}/${e.name}`;
-        return e;
+  router.get("/", (req, res) => {
+    db.get()
+      .find({ type: "Namespace" })
+      .then((resp) => resp.toArray())
+      .then((resp) =>
+        resp.map((e) => {
+          e.url = `${api.url}/${e.name}`;
+          return e;
+        })
+      )
+      .then((resp) => {
+        res.status(200).json({
+          data: resp,
+        });
+      })
+      .catch((err) => {
+        logger.error("Received error from MongoDB", {
+          in: "GET /",
+          error: err,
+        });
+        res.status(500).send("Internal Server Error");
       });
-      res.status(200).json({
-        data: resp,
-      });
-    } catch (err) {
-      logger.error(
-        "Received error from MongoDB",
-        "level",
-        "GET /",
-        "response",
-        err
-      );
-      next(err);
-    }
   });
 
   /**
    * GET all tracks from the API for a certain namespace from the API
    */
-  router.get("/:namespace", async (req, res, next) => {
-    try {
-      let resp = await db
-        .get()
-        .find({ type: "Track", namespace: req.params.namespace })
-        .toArray();
-      resp = resp.map((t) => {
-        t.url = `${api.url}/${req.params.namespace}/${t._id}/`;
-        t.waveform = `${api.url}/${req.params.namespace}/${t._id}/waveform`;
-        return t;
+  router.get("/:namespace", async (req, res) => {
+    db.get()
+      .find({ type: "Track", namespace: req.params.namespace })
+      .then((resp) => resp.toArray())
+      .then((resp) =>
+        resp.map((t) => {
+          t.url = `${api.url}/${req.params.namespace}/${t._id}/`;
+          t.waveform = `${api.url}/${req.params.namespace}/${t._id}/waveform`;
+          return t;
+        })
+      )
+      .then((resp) => {
+        res.status(200).json({
+          data: resp,
+        });
+      })
+      .catch((err) => {
+        logger.error("Received error from MongoDB", {
+          in: "GET /:namespace",
+          error: err,
+        });
+        res.status(500).send("Internal Server Error");
       });
-      res.status(200).json({
-        data: resp,
-      });
-    } catch (err) {
-      logger.error(
-        "Received error from MongoDB",
-        "level",
-        "GET /:namespace",
-        "response",
-        err
-      );
-      next(err);
-    }
   });
 
   /**
    * GET the cover of a specified namespace/album
    */
-  router.get("/:namespace/cover", async (req, res, next) => {
-    try {
-      let resp = await db
-        .get()
-        .findOne({ type: "Track", namespace: req.params.namespace });
-      res.redirect(resp.cover.public_url);
-    } catch (err) {
-      logger.error(
-        "Error while redirecting to cover",
-        "level",
-        "GET /:namespace/cover",
-        "namespace",
-        `${req.params.namespace}`,
-        "error",
-        err
-      );
-      next(err);
-    }
+  router.get("/:namespace/cover", async (req, res) => {
+    db.get()
+      .findOne({ type: "Track", namespace: req.params.namespace })
+      .then((resp) => {
+        res.redirect(resp.cover.public_url);
+      })
+      .catch((err) => {
+        logger.error("Error while redirecting to cover", {
+          in: "GET /:namespace/cover",
+          namespace: `${req.params.namespace}`,
+          error: err,
+        });
+        res.status(500).send("Internal Server Error");
+      });
   });
 
-  router.route("/:namespace/waveform").get(async (req, res, next) => {
-    try {
-      let resp = await db
-        .get()
-        .aggregate([
-          { $match: { type: "Waveform", namespace: req.params.namespace } },
-          {
-            $lookup: {
-              from: "demo",
-              localField: "track_id",
-              foreignField: "_id",
-              as: "track",
-            },
+  router.route("/:namespace/waveform").get((req, res) => {
+    db.get()
+      .aggregate([
+        { $match: { type: "Waveform", namespace: req.params.namespace } },
+        {
+          $lookup: {
+            from: "demo",
+            localField: "track_id",
+            foreignField: "_id",
+            as: "track",
           },
-        ])
-        .toArray();
-      resp = resp.map((waveform) => {
-        switch (req.query.mode) {
-          case "small":
-            return { waveform: waveform.small };
-          case "full":
-            return { waveform: waveform.full };
-          default:
-            return { waveform: { full: waveform.full, small: waveform.small } };
-        }
+        },
+      ])
+      .then((resp) => resp.toArray())
+      .then((resp) =>
+        resp.map((waveform) => {
+          switch (req.query.mode) {
+            case "small":
+              return { waveform: waveform.small };
+            case "full":
+              return { waveform: waveform.full };
+            default:
+              return {
+                waveform: { full: waveform.full, small: waveform.small },
+              };
+          }
+        })
+      )
+      .then((resp) => {
+        res.json({ data: resp });
+      })
+      .catch((err) => {
+        logger.error("Error while loading waveforms", {
+          in: "GET /:namespace/waveform",
+          namespace: `${req.params.namespace}`,
+          error: err,
+        });
+        res.status(500).send("Internal Server Error");
       });
-      res.json({ data: resp });
-    } catch (err) {
-      logger.error(
-        "Error while loading waveforms",
-        "level",
-        "GET /:namespace/waveform",
-        "namespace",
-        `${req.params.namespace}`,
-        "error",
-        err
-      );
-      next(err);
-    }
   });
 };
