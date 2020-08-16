2181b4fbc74962c42b61934135aeb82baf40a66f
Alexander Bartolomey <occloxium@gmail.com>
2020-08-16 20:15:32 +0200

    api: track: adopt logger API changes and switch to promises
    Replace async/await the same way as before with namespace (and all other functions)

--- api/lib/track/index.js
+++ api/lib/track/index.js
@@ -1,6 +1,6 @@
 const { guard, waveform, db } = require("../../util/");
 const { ObjectID } = require("mongodb");
-const logger = require("@zoomoid/log");
+const logger = require("@zoomoid/log").v2;
 const { api, waveman } = require("../../endpoints");
 
 module.exports = function (router) {
@@ -9,94 +9,82 @@ module.exports = function (router) {
     /**
      * Add new track to API and queries wave-man for waveforms
      */
-    .post(guard, async (req, res, next) => {
-      // logger.info('Received POST request on /file route');
-      try {
-        const track = req.body.track;
-        track.type = "Track";
-        track._id = new ObjectID();
-        waveform(track.namespace, track.filename, track._id, waveman.url);
-        const resp = await db.get().insertOne(track);
-        logger.info(
-          "Added track to namespace",
-          "level",
-          "POST /track",
-          "namespace",
-          track.namespace
-        );
-        res.status(200).json({
-          response: resp,
+    .post(guard, async (req, res) => {
+      const track = {
+        _id: new ObjectID(),
+        type: "Track",
+        ...req.body.track,
+      };
+      waveform(track.namespace, track.filename, track._id, waveman.url);
+      db.get()
+        .insertOne(track)
+        .then((resp) => {
+          logger.info("Added track to namespace", {
+            in: "POST /track",
+            namespace: track.namespace,
+          });
+          res.status(200).json({
+            response: resp,
+          });
+        })
+        .catch((err) => {
+          logger.error("Received error from MongoDB", {
+            in: "POST /track",
+            error: err,
+          });
+          res.status(500).send("Internal Server Error");
         });
-      } catch (err) {
-        logger.error(
-          "Received error from MongoDB",
-          "level",
-          "POST /track",
-          "response",
-          err
-        );
-        next(err);
-      }
     })
     /**
      * DELETE track from API
      */
-    .delete(guard, async (req, res, next) => {
-      try {
-        const resp = await db
-          .get()
-          .deleteMany({ path: req.body.path, type: "Track" });
-        logger.info(
-          "Deleted track from namespace",
-          "level",
-          "DELETE /track",
-          "track",
-          `${req.body.path}`
-        );
-        res.status(200).json({
-          response: resp,
+    .delete(guard, async (req, res) => {
+      db.get()
+        .deleteMany({ path: req.body.path, type: "Track" })
+        .then((resp) => {
+          logger.info("Deleted track from namespace", {
+            in: "DELETE /track",
+            track: `${req.body.path}`,
+          });
+          res.status(200).json({
+            response: resp,
+          });
+        })
+        .catch((err) => {
+          logger.error("Received error from MongoDB", {
+            in: "DELETE /track",
+            error: err,
+          });
+          res.status(500).send("Internal Server Error");
         });
-      } catch (err) {
-        logger.error(
-          "Received error from MongoDB",
-          "level",
-          "DELETE /track",
-          "response",
-          err
-        );
-        next(err);
-      }
     });
 
   /**
    * GET a specific track from the API
    */
-  router.get("/:namespace/:track", async (req, res, next) => {
-    try {
-      const resp = await db.get().findOne({
+  router.get("/:namespace/:track", async (req, res) => {
+    db.get()
+      .findOne({
         type: "Track",
         namespace: req.params.namespace,
         _id: ObjectID.createFromHexString(req.params.track),
+      })
+      .then((resp) => {
+        if (resp) {
+          resp.waveform = `${api.url}/${req.params.namespace}/${resp._id}/waveform`;
+          res.json(resp);
+        } else {
+          res.status(404).send("Not found");
+        }
+      })
+      .catch((err) => {
+        logger.error("Error while loading track", {
+          in: "GET /:namespace/:track",
+          namespace: `${req.params.namespace}`,
+          "track.id": `${req.params.track}`,
+          error: err,
+        });
+        res.status(500).send("Internal Server Error");
       });
-      if (resp) {
-        resp.waveform = `${api.url}/${req.params.namespace}/${resp._id}/waveform`;
-        res.json(resp);
-      } else {
-        res.status(404).send("Not found");
-      }
-    } catch (err) {
-      logger.error(
-        "Error while loading track",
-        "level",
-        "GET /:namespace/:track",
-        "namespace",
-        `${req.params.namespace}`,
-        "track.id",
-        `${req.params.track}`,
-        "error",
-        err
-      );
-      next(err);
-    }
   });
 };
