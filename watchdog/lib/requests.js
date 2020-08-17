64eed8b954bc4780b41d562a1a116194f226ea82
Alexander Bartolomey <occloxium@gmail.com>
2020-08-17 12:44:57 +0200

    watchdog: run prettier

--- watchdog/lib/requests.js
+++ watchdog/lib/requests.js
@@ -9,19 +9,21 @@ const { apiEndpoint } = require("../index");
  */
 async function removeTrack(p) {
   logger.info("Deleting track...", { track: p, endpoint: apiEndpoint });
-  return remove(`${apiEndpoint}/track`, { path: p }).then((resp) => {
-    if(resp.ok) {
-      logger.info("Deleted track", { track: `${path.basename(path)}` });
-    } else {
-      throw new Error(resp.statusText);
-    }
-  }).catch((err) => {
-    logger.error("Received error status from API", {
-      track: `${path.basename(path)}`,
-      in: "removeTrack",
-      error: err,
+  return remove(`${apiEndpoint}/track`, { path: p })
+    .then((resp) => {
+      if (resp.ok) {
+        logger.info("Deleted track", { track: `${path.basename(path)}` });
+      } else {
+        throw new Error(resp.statusText);
+      }
+    })
+    .catch((err) => {
+      logger.error("Received error status from API", {
+        track: `${path.basename(path)}`,
+        in: "removeTrack",
+        error: err,
+      });
     });
-  });
 }
 
 /**
@@ -33,19 +35,21 @@ async function removeNamespace(ns) {
     namespace: ns,
     endpoint: apiEndpoint,
   });
-  return remove(`${apiEndpoint}/namespace`, { namespace: ns }).then((resp) => {
-    if (resp.ok) {
-      logger.info("Deleted namespace", { namespace: ns });
-    } else {
-      throw new Error(resp.statusText);
-    }
-  }).catch((err) => {
-    logger.error("Received error status from API", {
-      namespace: ns,
-      in: "removeNamespace",
-      error: err,
+  return remove(`${apiEndpoint}/namespace`, { namespace: ns })
+    .then((resp) => {
+      if (resp.ok) {
+        logger.info("Deleted namespace", { namespace: ns });
+      } else {
+        throw new Error(resp.statusText);
+      }
+    })
+    .catch((err) => {
+      logger.error("Received error status from API", {
+        namespace: ns,
+        in: "removeNamespace",
+        error: err,
+      });
     });
-  });
 }
 
 /**
@@ -57,20 +61,21 @@ async function addNamespace(namespace) {
     namespace: namespace,
     endpoint: apiEndpoint,
   });
-  return add(`${apiEndpoint}/namespace`, { namespace }).then((resp) => {
-    if(resp.ok) {
-      logger.info("Added namespace", { namespace: namespace });
-
-    } else {
-      throw new Error(resp.statusText);
-    }
-  }).catch((err) => {
-    logger.error("Received error status from API", {
-      namespace: namespace,
-      in: "addNamespace",
-      error: err,
+  return add(`${apiEndpoint}/namespace`, { namespace })
+    .then((resp) => {
+      if (resp.ok) {
+        logger.info("Added namespace", { namespace: namespace });
+      } else {
+        throw new Error(resp.statusText);
+      }
+    })
+    .catch((err) => {
+      logger.error("Received error status from API", {
+        namespace: namespace,
+        in: "addNamespace",
+        error: err,
+      });
     });
-  });
 }
 
 /**
@@ -82,19 +87,21 @@ async function addTrack(track) {
     track: track.filename,
     endpoint: apiEndpoint,
   });
-  return add(`${apiEndpoint}/track`, { track: track }).then((resp) => {
-    if (resp.ok) {
-      logger.info("Added track", { track: track.filename });
-    } else {
-      throw new Error(resp.statusText);
-    }
-  }).catch((err) => {
-    logger.error("Received error status from API", {
-      namespace: track.filename,
-      in: "addTrack",
-      error: err,
+  return add(`${apiEndpoint}/track`, { track: track })
+    .then((resp) => {
+      if (resp.ok) {
+        logger.info("Added track", { track: track.filename });
+      } else {
+        throw new Error(resp.statusText);
+      }
+    })
+    .catch((err) => {
+      logger.error("Received error status from API", {
+        namespace: track.filename,
+        in: "addTrack",
+        error: err,
+      });
     });
-  });
 }
 
 async function changeMetadata(o, p) {
@@ -102,24 +109,26 @@ async function changeMetadata(o, p) {
     path: p,
     data: o,
   });
-  return change(`${apiEndpoint}/namespace/metadata`, {metadata: o}).then((resp) => {
-    if (resp.ok) {
-      logger.info("Updated namespace metadata", {
+  return change(`${apiEndpoint}/namespace/metadata`, { metadata: o })
+    .then((resp) => {
+      if (resp.ok) {
+        logger.info("Updated namespace metadata", {
+          namespace: o.namespace,
+          path: p,
+          data: o,
+        });
+      } else {
+        throw new Error(resp.statusText);
+      }
+    })
+    .catch((err) => {
+      logger.error("Received error status from API", {
         namespace: o.namespace,
         path: p,
-        data: o,
+        in: "changeMetadata",
+        error: err,
       });
-    } else {
-      throw new Error(resp.statusText);
-    }
-  }).catch((err) => {
-    logger.error("Received error status from API", {
-      namespace: o.namespace,
-      path: p,
-      in: "changeMetadata",
-      error: err,
     });
-  });
 }
 
 module.exports = {
