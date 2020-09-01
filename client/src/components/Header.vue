bb67dc19ae487898ddc6b68a06ded32fa2e8ec68
Alexander Bartolomey <occloxium@gmail.com>
2020-09-01 15:20:26 +0200

    :sparkles: client: add dedicated header component
    demo-man logo needs some reworking, as currently, the colors clash

---
+++ client/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <div class="backdrop"></div>
+    <img class="logo" src="~@/assets/demo-man.png" />
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+header
+  position: relative
+.logo
+  width: 100%
+  max-width: 350px
+  margin: 0 auto
+  display: block
+  position: relative
+  z-index: 1
+  padding-top: 8em
+  padding-bottom: 12em
+  user-select: none
+.backdrop
+  position: absolute
+  z-index: 0
+  top: 0
+  left: 0
+  right: 0
+  bottom: 0
+  transform: skewY(-5deg) scaleY(1.3) translateY(-50%)
+  transform-origin: 100% 0
+  background-color: #1a1a1a
+</style>
