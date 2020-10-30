97122219224708ac0b23c28ede238dfbe44e54a6
Alexander Bartolomey <occloxium@gmail.com>
2020-10-30 11:30:20 +0100

    refactor: :recycle: update patch utility
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- tag.py
+++ tag.py
@@ -1,4 +1,3 @@
-#!C:\Python38\python.exe
 import argparse
 import subprocess
 
@@ -9,53 +8,39 @@ parser.add_argument("--message", required=False)
 args = parser.parse_args()
 
 def main():
-    try:
-        with open(".version", "rt+") as f:
-            version = f.read().split(".")
-            if len(version) != 3:
-                print("❌ Corrupted version file encountered")
-                print("🚪 Exiting.")
-                return 1
-            v=version_str(version)
-            if args.type in ["major", "maj", "M"]:
-                print("👑 Adding new major version")
-                message = args.message if args.message else "🎉✨ New major version"
-                version[2] = "0"
-                version[1] = "0"
-                version[0] = str(int(version[0]) + 1)
-                v = git_hook(version, message)
-                f.write(v)
-            elif args.type in ["minor", "min", "m"]:
-                print("✨ Adding new minor version")
-                message = args.message if args.message else "💡🔥 New minor version"
-                version[2] = "0"
-                version[1] = str(int(version[1]) + 1)
-                v = git_hook(version, message)
-                f.write(v)
-            elif args.type in ["patch", ""]:
-                print("🩹 Adding new patch version")
-                message = args.message if args.message else "🩹♻️ Patch"
-                version[2] = str(int(version[2]) + 1)
-                v = git_hook(version, message)
-                f.write(v)
-            elif args.type in ["get"]:
-                v = subprocess.check_output(["git", "describe"], text=True).split("-")[0].replace("v", "").strip()
-                print(v)
-                f.write(v)
-                f.close()
-                return
-            else:
-                print("❌ Invalid version type supplied")
-                print("🙅 Not creating new version tag")
-            print("📦 Version stands at {}".format(v))
-            print("🚪 Exiting.")
-    except IOError:
-        print("❌ .version file not present!")
-        print("🖌 creating it from current git tag")
-        f = open(".version", "wt")
-        f.write(
-            subprocess.check_output(["git", "describe"], text=True).split("-")[0].replace("v", "").strip())
-        f.close()
+    version = subprocess.check_output(["git", "describe"], text=True).split("-")[0].replace("v", "").strip().split(".")
+    if len(version) != 3:
+        print("❌ Illformated semantic version tag encountered")
+        print("💁‍♀️ Please create a valid semantic version tag manually and push it!")
+        print("🚪 Exiting.")
+        return 1
+    if args.type in ["get"]:
+        print(version_str(version))
+        return 0
+    if args.type in ["major", "maj", "M"]:
+        print("👑 Adding new major version")
+        message = args.message if args.message else "🎉✨ New major version"
+        version[2] = "0"
+        version[1] = "0"
+        version[0] = str(int(version[0]) + 1)
+        v = git_hook(version, message)
+    elif args.type in ["minor", "min", "m"]:
+        print("✨ Adding new minor version")
+        message = args.message if args.message else "💡🔥 New minor version"
+        version[2] = "0"
+        version[1] = str(int(version[1]) + 1)
+        v = git_hook(version, message)
+    elif args.type in ["patch", ""]:
+        print("🩹 Adding new patch version")
+        message = args.message if args.message else "🩹♻️ New patch"
+        version[2] = str(int(version[2]) + 1)
+        v = git_hook(version, message)
+    else:
+        print("❌ Invalid version type supplied")
+        print("🙅 Not creating new version tag")
+        return 3
+    print("📦 Version stands at {}".format(v))
+    print("🚪 Exiting.")
 
 def git_hook(version, message):
     v = version_str(version)
