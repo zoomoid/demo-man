92d97a648d8da48aaf74ec8e50b18ed9cfbed96a
Alexander Bartolomey <occloxium@gmail.com>
2020-12-13 15:40:42 +0100

    feat(picasso): :tada: add "picasso" micro-service
    Converts a given cover image into a palette of dominant colors for computed themes

---
+++ picasso/main.py
@@ -0,0 +1,51 @@
+from flask import Flask, request
+import uuid
+import json
+import os
+from colorthief import ColorThief
+
+app = Flask(__name__)
+
+"""
+Handler for service discovery
+"""
+@app.route("/", methods=["GET"])
+def waveman():
+  return {"app": "demo-man", "svc": "picasso", "version": os.environ["VERSION"]}
+
+
+"""
+Cosmetic GET route to health-check the service. Used by Kubernetes' pod controller to check pod
+health
+"""
+@app.route("/healthz", methods=["GET"])
+def healthz():
+  return "ok"
+
+"""
+Cosmetic GET route to check the service. Can be used by users to check the responsiveness of the service
+"""
+@app.route("/ping", methods=["GET"])
+def ping():
+  return "pong"
+
+"""
+Returns the dominant color palette of an image
+"""
+@app.route("/palette/<no>", methods=["POST"])
+def palette(no):
+  image_url = request.json["url"]
+  path = f"{os.environ["VOLUME"]}{image_url}"
+  palette = ColorThief(path).get_palette(color_count=no, quality=5)
+  return {"palette": palette}
+
+
+"""
+Returns the dominant color palette of an image
+"""
+@app.route("/color/<quality>", methods=["POST"])
+def palette(quality):
+  image_url = request.json["url"]
+  path = f"{os.environ["VOLUME"]}{image_url}"
+  palette = ColorThief(path).get_color(quality=quality)
+  return {"color": color}
