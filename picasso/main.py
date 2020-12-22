11f8e27e63deded6708659fcfbe8497af682e48d
Alexander Bartolomey <occloxium@gmail.com>
2020-12-22 18:47:23 +0100

    refactor(picasso): :recycle: converge theme objects in waveman and picasso and add color logic into own module

--- picasso/main.py
+++ picasso/main.py
@@ -3,51 +3,76 @@ import uuid
 import json
 import os
 from colorthief import ColorThief
-from . import utils
+from utils.colors import determine_text_color
+
 
 app = Flask(__name__)
 
-"""
-Handler for service discovery
-"""
 @app.route("/", methods=["GET"])
-def waveman():
-  return {"app": "demo-man", "svc": "picasso", "version": os.environ["VERSION"]}
+def picasso() -> dict:
+    """Handler for service discovery
 
+    :returns: picasso service descriptor
+    :rtype: dict
+    """
+    return {"app": "demo-man", "svc": "picasso", "version": os.environ["VERSION"]}
 
-"""
-Cosmetic GET route to health-check the service. Used by Kubernetes' pod controller to check pod
-health
-"""
 @app.route("/healthz", methods=["GET"])
-def healthz():
-  return "ok"
+def healthz() -> str:
+    """Cosmetic GET route to health-check the service. Used by Kubernetes' pod controller to 
+    check pod health
 
-"""
-Cosmetic GET route to check the service. Can be used by users to check the responsiveness of the service
-"""
+    :returns: ok
+    :rtype: str
+    """
+    return "ok"
+
 @app.route("/ping", methods=["GET"])
-def ping():
-  return "pong"
+def ping() -> str:
+    """Cosmetic GET route to check the service. Can be used by users to check the responsiveness 
+    of the service
 
-"""
-Returns the dominant color palette of an image
-"""
+    :returns: pong
+    :rtype: str
+    """
+    return "pong"
+
 @app.route("/palette/<no>", methods=["POST"])
-def palette(no):
-  image_url = request.json["url"]
-  print(f"Calculating color palette for cover at {image_url}")
-  path = f"{os.environ['VOLUME']}{image_url}"
-  palette = ColorThief(path).get_palette(color_count=int(no), quality=5)
-  print(f"Calculated palette {palette}")
-  return {"palette": palette}
+def palette(no: str) -> dict:
+    """Returns the dominant color palette of an image
 
-"""
-Returns the dominant color palette of an image
-"""
+    :param no: number of colors to calculate in the palette
+    :type no: int
+    :returns: theme object for API to receive
+    :rtype: dict
+    """
+    image_url = request.json["url"]
+    print(f"Calculating color palette for cover at {image_url}")
+    path = f"{os.environ['VOLUME']}{image_url}"
+    palette = ColorThief(path).get_palette(color_count=int(no), quality=5)
+    color = ColorThief(path).get_color(quality=5)
+    print(f"Calculated palette {palette}")
+    return {
+        "color": color,
+        "textColor": determine_text_color(color),
+        "accent": palette[1],
+        "palette": palette,
+    }
+
 @app.route("/color/<quality>", methods=["POST"])
-def color(quality):
-  image_url = request.json["url"]
-  path = f"{os.environ['VOLUME']}{image_url}"
-  color = ColorThief(path).get_color(quality=quality)
-  return {"color": color}
+def color(quality: str) -> dict:
+    """Returns the dominant color palette of an image
+
+    :param quality: ColorThief quality parameter
+    :type quality: int
+    :returns: theme object for API to receive
+    :rtype: dict
+    """
+    image_url = request.json["url"]
+    path = f"{os.environ['VOLUME']}{image_url}"
+    color = ColorThief(path).get_color(quality=quality)
+    return {
+        "color": color,
+        "textColor": determine_text_color(color),
+        "accent": None
+    }
