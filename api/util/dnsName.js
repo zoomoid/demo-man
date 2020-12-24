869585339f850dd86307a98ebdd5f3ab2f29768a
Alexander Bartolomey <occloxium@gmail.com>
2020-12-24 01:42:55 +0100

    refactor(api): :recycle: refactor dnsName normalizer

--- api/util/dnsName.js
+++ api/util/dnsName.js
@@ -8,6 +8,7 @@ module.exports = function dnsName(name = "") {
     .toLowerCase() // convert to lowercase
     .replace(" ", "-") // replace spaces with -
     .replace("_", "-") // replace other separation characters
-    .replace(/[^a-z0-9]/, "") // remove all non-lowercase letters or non-numbers
+    .replace(".", "-") // replace other separation characters
+    .replace(/[^a-z0-9-]/, "") // remove all non-lowercase letters or non-numbers
     .substr(0, 63); // limit length to maximum of 64 chars
 };
