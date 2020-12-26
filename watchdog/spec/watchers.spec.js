2ff435baa0f1cd2cd35b001befe1a23e549b07ba
Alexander Bartolomey <occloxium@gmail.com>
2020-12-26 01:34:41 +0100

    test(watchdog): :white_check_mark: update experimental watcher test

--- watchdog/spec/watchers.spec.js
+++ watchdog/spec/watchers.spec.js
@@ -1,13 +1,22 @@
-const { describe, it, after } = require("mocha");
+const { describe, it, after, before } = require("mocha");
 const { expect } = require("chai");
 const fs = require("fs");
 const { buildWatchers } = require("../lib/watchers");
 
 const watcherFactory = () => {
-  return buildWatchers("./data");
+  return buildWatchers("/git/web/demo-man/watchdog/dist/").watchers;
 };
 
 describe("Namespace watcher test", function () {
+  this.timeout(6000);
+  before(function (done) {
+    try {
+      fs.mkdirSync("./dist/");
+      done();
+    } catch (err) {
+      done(err);
+    }
+  });
   it("Valid namespace (1)", function (done) {
     const w = watcherFactory();
     const p = "voyager";
@@ -15,61 +24,64 @@ describe("Namespace watcher test", function () {
       expect(createdPath).to.contain(p);
       done();
     });
-    fs.mkdirSync(p);
+    fs.mkdirSync("./dist/" + p);
+    setTimeout(() => done(new Error("FSWatcher did not fire, when it should have")), 5000);
   });
-  it("Valid namespace (2)", function (done) {
-    const w = watcherFactory();
-    const p = "shades-of-yellow";
-    w.namespace.on("addDir", function (createdPath) {
-      expect(createdPath).to.contain(p);
-      done();
-    });
-    fs.mkdirSync(p);
-  });
-  it("Valid namespace (3)", function (done) {
-    const w = watcherFactory();
-    const p = "shades_of_yellow";
-    w.namespace.on("addDir", function (createdPath) {
-      expect(createdPath).to.contain(p);
-      done();
-    });
-    fs.mkdirSync(p);
-  });
-  it("Valid namespace (4)", function (done) {
-    const w = watcherFactory();
-    const p = "Voyager";
-    w.namespace.on("addDir", function (createdPath) {
-      expect(createdPath).to.contain(p);
-      done();
-    });
-    fs.mkdirSync(p);
-  });
-  it("Invalid namespace (1)", function (done) {
-    const w = watcherFactory();
-    const p = "Shades Of Yellow";
-    w.namespace.on("addDir", function () {
-      done(false);
-    });
-    // Should not be picked up by the watcher
-    fs.mkdirSync(p);
-    this.timeout(2000); // should be picked up by the watcher in less time if correct
-    done();
-  });
-  it("Invalid namespace (2)", function (done) {
-    const w = watcherFactory();
-    const p = ".shades-of-yellow";
-    w.namespace.on("addDir", function () {
-      done(false);
-    });
-    // Should not be picked up by the watcher
-    fs.mkdirSync(p);
-    this.timeout(2000); // should be picked up by the watcher in less time if correct
-    done();
-  });
-  after(function () {
-    fs.rmSync("./data", {
+  // it("Valid namespace (2)", function (done) {
+  //   const w = watcherFactory();
+  //   const p = "shades-of-yellow";
+  //   w.namespace.on("addDir", function (createdPath) {
+  //     expect(createdPath).to.contain(p);
+  //     done();
+  //   });
+  //   fs.mkdirSync("./dist/" + p);
+  //   setTimeout(() => done(new Error("FSWatcher did not fire, when it should have")), 5000);
+  // });
+  // it("Valid namespace (3)", function (done) {
+  //   const w = watcherFactory();
+  //   const p = "shades_of_yellow";
+  //   w.namespace.on("addDir", function (createdPath) {
+  //     expect(createdPath).to.contain(p);
+  //     done();
+  //   });
+  //   fs.mkdirSync("./dist/" + p);
+  //   setTimeout(() => done(new Error("FSWatcher did not fire, when it should have")), 5000);
+  // });
+  // it("Valid namespace (4)", function (done) {
+  //   const w = watcherFactory();
+  //   const p = "Voyager";
+  //   w.namespace.on("addDir", function (createdPath) {
+  //     expect(createdPath).to.contain(p);
+  //     done();
+  //   });
+  //   fs.mkdirSync("./dist/" + p);
+  //   setTimeout(() => done(new Error("FSWatcher did not fire, when it should have")), 5000);
+  // });
+  // it("Invalid namespace (1)", function (done) {
+  //   const w = watcherFactory();
+  //   const p = "Shades Of Yellow";
+  //   w.namespace.on("addDir", function () {
+  //     done(false);
+  //   });
+  //   // Should not be picked up by the watcher
+  //   fs.mkdirSync("./dist/" + p);
+  //   setTimeout(() => done(), 2000);
+  // });
+  // it("Invalid namespace (2)", function (done) {
+  //   const w = watcherFactory();
+  //   const p = ".shades-of-yellow";
+  //   w.namespace.on("addDir", function () {
+  //     done(false);
+  //   });
+  //   // Should not be picked up by the watcher
+  //   fs.mkdirSync("./dist/" + p);
+  //   setTimeout(() => done(), 2000);
+  // });
+  after(function (done) {
+    fs.rmSync("./dist", {
       recursive: true,
     });
+    done();
   });
 });
 
