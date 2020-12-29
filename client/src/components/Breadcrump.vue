f25816dcbe9790e1028987022649241a967bcf7f
Alexander Bartolomey <occloxium@gmail.com>
2020-12-30 00:25:32 +0100

    refactor(client): :art: update Breadcrump.vue

--- client/src/components/Breadcrump.vue
+++ client/src/components/Breadcrump.vue
@@ -1,37 +1,46 @@
 <template>
-  <div class="breadcrump">
+  <div
+    class="breadcrump"
+    :style="{
+      '--accent': theme.accent,
+      '--textColor': theme.textColor,
+      '--color': theme.color,
+    }"
+  >
     <router-link to="/">
-      <i class="material-icons-sharp">
-        arrow_back
-      </i>
+      <i class="material-icons-sharp"> arrow_back </i>
     </router-link>
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    theme: {
+      type: Object,
+      default: () => ({
+        textColor: null,
+        color: null,
+        accent: null
+      })
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped>
-.breadcrump {
-  padding: 1em 2em 1em 2em;
-  a {
-    color: inherit;
-    .material-icons-sharp {
-      vertical-align: middle;
-      line-height: 32px;
-      width: 32px;
-      height: 32px;
-      text-align: center;
-      &:hover, &:active, .muted {
-        background: rgba(255,255,255,0.2);
-        text-shadow: 0 2px 2px rgba(0,0,0,0.2);
-        border-radius: 32px;
-      }
-    }
-
-  }
-}
+<style lang="sass" scoped>
+.breadcrump
+  padding: 1em 2em 1em 2em
+  color: rgba(var(--textColor),1)
+  a
+    color: inherit
+    .material-icons-sharp
+      vertical-align: middle
+      line-height: 32px
+      width: 32px
+      height: 32px
+      text-align: center
+      &:hover, &:active, .muted
+        text-shadow: 0 2px 2px rgba(0,0,0,0.2)
+        border-radius: 32px
 </style>
