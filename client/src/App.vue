e7f5c4b58d8fd0101d1bf34ef1db75f1d9fbb3f3
Alexander Bartolomey <occloxium@gmail.com>
2020-12-30 00:27:45 +0100

    refactor(client): :recycle: refactor App scaffolding

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,38 +1,25 @@
 <template>
-  <div id="app">
+  <div id="app" class="">
     <GlobalPlayer></GlobalPlayer>
-    <transition name="fade">
-      <keep-alive>
-        <router-view></router-view>
-      </keep-alive>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
     </transition>
   </div>
 </template>
 
 <style lang="scss">
-@import url("https://fonts.googleapis.com/css?family=IBM+Plex+Sans:300,500,700&display=swap");
+@import url("https://use.typekit.net/kxk3lzb.css");
 
-html {
-  font-family: "IBM Plex Sans", sans-serif;
-  letter-spacing: -0.03em;
-  @media screen and (max-width: 768px) {
-    font-size: 10pt;
-  }
-  // background: #242424;
-}
-body {
-  margin: 0;
-}
-* {
-  box-sizing: border-box;
-}
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity 0.1s ease;
+  transition: opacity 0.05s ease;
 }
 .fade-enter,
 .fade-leave-active {
   opacity: 0;
+}
+html {
+  @apply bg-white;
 }
 </style>
 
