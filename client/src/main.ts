22999e03a32ffe0d3478a1d20f90e2c7b634ed95
Alexander Bartolomey <occloxium@gmail.com>
2020-12-30 23:49:22 +0100

    refactor(client): :recycle: refactor main.ts and add correct shims for .vue files

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,52 +1,28 @@
-import Vue from "vue";
+import { createApp, onMounted } from "vue";
 
-declare module "vue/types/vue" {
-  // Global properties can be declared
-  // on the `VueConstructor` interface
-  interface VueConstructor {
-    $version?: string;
-    $publicEP?: string;
-    $apiEP?: string;
-  }
-}
+import router from "./router";
+import { useStore } from "./store";
 
-import VueRouter from "vue-router";
-import App from "./App.vue";
-import Home from "./views/Home.vue";
-import Page from "./views/Page.vue";
-import "./registerServiceWorker";
-import store from "./store";
 import "./assets/css/tailwind.scss";
+import "./registerServiceWorker";
 
-Vue.use(VueRouter);
+const app = createApp({
+  setup() {
+    const store = useStore();
 
-const routes = [
-  {
-    path: "/",
-    component: Home
-  },
-  {
-    path: "/:id",
-    component: Page
+    onMounted(() => {
+      console.log(`Running on version ${process.env.VERSION}`);
+    });
+
+    return { store };
   }
-];
-
-const router = new VueRouter({
-  mode: "history",
-  routes
 });
 
-Vue.config.productionTip = false;
+app.config.globalProperties.version = process.env.VERSION;
+app.config.globalProperties.baseUrl = process.env.BASE_URL;
+app.config.globalProperties.apiUrl = process.env.API_URL;
 
-Vue.$version = process.env.VERSION;
-Vue.$apiEP = process.env.API_URL;
-Vue.$publicEP = process.env.BASE_URL;
+app.use(router);
+app.mount("#app");
 
-export default new Vue({
-  router,
-  render: h => h(App),
-  store,
-  created() {
-    console.log(`Currently running version ${Vue.$version}`);
-  }
-}).$mount("#app");
+export default app;
