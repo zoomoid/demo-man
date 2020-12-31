598f80f855d9fd9771c6b667496eab208717a3c7
Alexander Bartolomey <occloxium@gmail.com>
2020-12-31 14:35:49 +0100

    refactor(client): :recycle: inject global constants for version, API URL and Base URL and provide global functions in main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -18,11 +18,27 @@ const app = createApp({
   }
 });
 
-app.config.globalProperties.version = process.env.VERSION;
-app.config.globalProperties.baseUrl = process.env.BASE_URL;
-app.config.globalProperties.apiUrl = process.env.API_URL;
+app.provide("version", process.env.VERSION);
+app.provide("baseUrl", process.env.BASE_URL);
+app.provide("apiUrl", process.env.API_URL);
 
 app.use(router);
 app.mount("#app");
 
 export default app;
+
+export function toTime(val: number): string {
+  try {
+    const hhmmss = new Date(val * 1000).toISOString().substr(11, 8);
+    return hhmmss.indexOf("00:") === 0 ? hhmmss.substr(3) : hhmmss;
+  } catch (e) {
+    return "00:00";
+  }
+}
+
+export function rgbToHex(color: string): string {
+  return color
+    .split(", ")
+    .map((c) => parseInt(c, 10).toString(16))
+    .reduce((p, c) => p + c);
+}
