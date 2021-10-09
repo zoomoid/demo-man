3ad0672c4da16402f73492a8ce2ec7e56bc298b6
Alexander Bartolomey <occloxium@gmail.com>
2021-10-09 16:26:12 +0200

    chore: removed unused specification for update-track.input.ts
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- api/src/tracks/dto/update-track.input.ts
+++ api/src/tracks/dto/update-track.input.ts
@@ -1,8 +1,5 @@
 import { CreateTrackInput } from './create-track.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateTrackInput extends PartialType(CreateTrackInput) {
-  @Field(() => Int)
-  id: number;
-}
+export class UpdateTrackInput extends PartialType(CreateTrackInput) {}
