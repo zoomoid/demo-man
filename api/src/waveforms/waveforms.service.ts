8a1abb92205b55f5edb728a14fd25b9b9e804b63
Alexander Bartolomey <occloxium@gmail.com>
2021-11-01 19:49:49 +0100

    feat: start waveform service implementation
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- api/src/waveforms/waveforms.service.ts
+++ api/src/waveforms/waveforms.service.ts
@@ -1,23 +1,51 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Version } from '../versions/entities/version.entity';
 import { CreateWaveformInput } from './dto/create-waveform.input';
 import { UpdateWaveformInput } from './dto/update-waveform.input';
+import { Waveform } from './entities/waveform.entity';
 
 @Injectable()
 export class WaveformsService {
-  create(createWaveformInput: CreateWaveformInput) {
-    return 'This action adds a new waveform';
+  constructor(
+    @InjectRepository(Waveform)
+    private waveformRepository: Repository<Waveform>,
+
+    @InjectRepository(Version)
+    private versionRepository: Repository<Version>,
+  ) {}
+
+  async create(createWaveformInput: CreateWaveformInput) {
+    const waveform = new Waveform();
+    waveform.full = createWaveformInput.full;
+    waveform.small = createWaveformInput.small;
+
+    const version = await this.versionRepository.findOne(
+      createWaveformInput.versionId,
+    );
+
+    waveform.version = version;
+
+    this.waveformRepository.save(waveform);
+
+    version.waveform = waveform;
+    this.versionRepository.save(version);
   }
 
-  findAll() {
-    return `This action returns all waveforms`;
+  async findAll() {
+    return await this.waveformRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} waveform`;
+  async findOne(id: string) {
+    return await this.waveformRepository.findOne(id);
   }
 
-  update(id: number, updateWaveformInput: UpdateWaveformInput) {
-    return `This action updates a #${id} waveform`;
+  async update(id: number, updateWaveformInput: UpdateWaveformInput) {
+    const waveform = await this.waveformRepository.findOne(id);
+    waveform.full = updateWaveformInput.full;
+    waveform.small = updateWaveformInput.small;
+    return await this.waveformRepository.update(id, waveform);
   }
 
   remove(id: number) {
