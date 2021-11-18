8f3420d726402550e77b182d3ba84751f2cb9a0c
Alexander Bartolomey <occloxium@gmail.com>
2021-11-18 21:55:14 +0100

    feat: implement rudimentary healthcheck endpoints
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -5,8 +5,18 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/healthz')
+  getHealthz(): string {
+    return this.appService.getHealthz();
+  }
+
+  @Get('/livez')
+  getLivez(): string {
+    return this.appService.getLivez();
+  }
+
+  @Get('/readyz')
+  getReadyz(): string {
+    return this.appService.getReadyz();
   }
 }
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -5,4 +5,16 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getHealthz(): string {
+    return 'OK';
+  }
+
+  getReadyz(): string {
+    return 'Ready';
+  }
+
+  getLivez(): string {
+    return 'Alive';
+  }
 }
