5b623521d6cd22f896345a7e2d92e726426989ff
Alexander Bartolomey <occloxium@gmail.com>
2023-11-09 00:09:17 +0100

    chore!: remove some old stuff
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- frontman/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './index.css'
-import { Buffer } from "buffer";
-
-(<any>window).Buffer = Buffer;
-
-createApp(App).mount('#app')
